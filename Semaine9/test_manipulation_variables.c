74c1e0bcd582f999ab75d7b3385d291f4e8066a1
Victor Lambret <lambret.victor@gmail.com>
2017-04-07 08:41:40 +0200

    Semaine10 : tdd prêt

--- Semaine9/test_manipulation_variables.c
+++ Semaine9/test_manipulation_variables.c
@@ -9,6 +9,16 @@ void appliquer_expansion_variables(char *ligne_originale,
                                    char *ligne_expansee);
 */
 
+#define CONCAT_BUFFER_SIZE 128
+char concat_buffer[CONCAT_BUFFER_SIZE];
+
+char *concat(char *testName, char *specialCase) {
+	strncpy(concat_buffer, testName, CONCAT_BUFFER_SIZE);
+	strcat(concat_buffer, specialCase);
+
+	return concat_buffer;
+}
+
 void test_trouver_helper(char * ligne,
                          int retourAttendu,
                          char * nomVariable,
@@ -21,17 +31,21 @@ void test_trouver_helper(char * ligne,
 	initialiser_variables();
 
 	retour = trouver_affectation_variable(ligne);
-	test_expect (retour == retourAttendu, testName);
+	test_expect (retour == retourAttendu, concat(testName, " trouver_affectation_variable"));
 
 	retour = trouver_variable(nomVariable);
-	test_expect (retour == retourAttendu, testName);
+	test_expect (retour == retourAttendu, concat(testName, " trouver_variable"));
 
 	valeurObtenue = valeur_variable(nomVariable);
 	retour = strcmp(valeurObtenue, valeurAttendue);
-	test_expect (retour == 0, testName);
+	test_expect (retour == 0, concat(testName, " valeur_variable"));
 }
 
 void test_manipulation_variables_complete()
 {
 	test_trouver_helper("", 0, "", "", "chaine vide");
+	test_trouver_helper("CC=gcc", 1, "CC", "gcc", "affectation normale");
+	test_trouver_helper("CC =gcc", 0, "CC", "", "espace avant egal");
+	test_trouver_helper("CC= gcc", 0, "CC", "", "espace apres egal");
+	test_trouver_helper("CC = gcc", 0, "CC", "", "espace autour du egal");
 }
