aa95f57a5e1bb4d16cc157aae3f025c8595ad81c
Victor Lambret <victor.lambret@sogilis.com>
2018-04-02 13:51:33 +0200

    Ajout code source TP7 question e

---
+++ corrections/TP7/instant_suivant10.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+typedef struct {
+	int heure;
+	int minute;
+	int seconde;
+} instant;
+
+void instant_suivant() {
+	instant instant_courant, instant_suivant;
+
+	scanf("%d:%d:%d", &instant_courant.heure,
+	                  &instant_courant.minute,
+	                  &instant_courant.seconde);
+
+	instant_suivant.seconde = instant_courant.seconde + 1;
+	if (instant_suivant.seconde == 60) {
+
+		instant_suivant.seconde = 0;
+		instant_suivant.minute = instant_courant.minute + 1;
+		if (instant_suivant.minute == 60) {
+
+		  instant_suivant.minute = 0;
+		  instant_suivant.heure = instant_courant.heure + 1;
+			if (instant_suivant.heure == 24) {
+				instant_suivant.heure = 0;
+			}
+
+		} else {
+			instant_suivant.heure = instant_courant.heure;
+		}
+
+	} else {
+	  instant_suivant.minute = instant_courant.minute;
+	  instant_suivant.heure = instant_courant.heure;
+	}
+
+	printf("nouvelle heure : %02d:%02d:%02d\n",
+	       instant_suivant.heure, instant_suivant.minute,
+	       instant_suivant.seconde);
+}
+
+int main() {
+	int i;
+	for (i = 0; i < 10 ; i++) {
+		instant_suivant();
+	}
+	return 0;
+}
